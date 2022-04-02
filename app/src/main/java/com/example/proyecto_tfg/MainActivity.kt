9437cf40b2274822bc123307ac2d4b1c59fea2a5
Antonio Termes <antoniotermesgiral@gmail.com>
2022-04-02 19:56:28 +0200

    Basic fragments created and news appears on application start

--- app/src/main/java/com/example/proyecto_tfg/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_tfg/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.proyecto_tfg
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.get
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.proyecto_tfg.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when (it.itemId) {
+
+                R.id.search_button -> replaceFragment(SearchFragment())
+                R.id.news_button -> replaceFragment(NewsFragment())
+                R.id.library_button -> replaceFragment(LibraryFragment())
+
+            }
+
+            return@setOnItemSelectedListener true
+
+        }
+
+        binding.bottomNavigationView.selectedItemId = R.id.news_button
+
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+
+        val fragmentManager : FragmentManager = supportFragmentManager
+        val fragmentTransaction : FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.main_frame_layout, fragment)
+        fragmentTransaction.commit()
+
+    }
+
 }
