f3c3298e07ea66ee979d14998698f5488d17b077
Antonio Termes <antoniotermesgiral@gmail.com>
2022-04-03 19:24:45 +0200

    Basic request to IGDB API

--- app/src/main/java/com/example/proyecto_tfg/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_tfg/MainActivity.kt
@@ -1,16 +1,34 @@
 package com.example.proyecto_tfg
 
+import android.Manifest
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.view.get
+import android.provider.MediaStore
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.example.proyecto_tfg.databinding.ActivityMainBinding
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.IOException
+import java.security.Permission
+import android.content.pm.PackageManager
+
+import androidx.core.app.ActivityCompat
+
+import android.os.Build
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding:ActivityMainBinding
+    lateinit var client: OkHttpClient
+    private val url = "https://api.igdb.com/v4/"
+    private val permissions = listOf(Manifest.permission.INTERNET, Manifest.permission.ACCESS_NETWORK_STATE)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +52,13 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.bottomNavigationView.selectedItemId = R.id.news_button
+        client = OkHttpClient()
+
+        if(comprobarPermisos()){
+
+            Thread { pruebaPost() }.start()
+
+        }
 
     }
 
@@ -43,6 +68,50 @@ class MainActivity : AppCompatActivity() {
         val fragmentTransaction : FragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.main_frame_layout, fragment)
         fragmentTransaction.commit()
+
+    }
+
+    fun pruebaPost() {
+
+        val markdownMediaType = "text/x-markdown; charset=utf-8".toMediaType()
+
+        val postBody = """
+            fields name;
+            search "Far";
+            limit 1;  
+            """.trimMargin()
+
+        val request = Request.Builder().url(url + "games").post(postBody.toRequestBody(markdownMediaType))
+            .addHeader("Client-ID", resources.getString(R.string.ClientID))
+            .addHeader("Authorization", resources.getString(R.string.Authorization)).build()
+
+        client.newCall(request).execute().use { response ->
+
+            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+            Log.d(":::",response.body!!.string())
+
+        }
+
+    }
+
+    private fun comprobarPermisos(): Boolean {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && permissions != null) {
+
+            for (permission in permissions) {
+
+                if (ActivityCompat.checkSelfPermission(this, permission!!) != PackageManager.PERMISSION_GRANTED ) {
+
+                    return false
+
+                }
+
+            }
+
+        }
+
+        return true
 
     }
 
